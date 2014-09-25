05a81276dab17628fc342b8b5a1c9a8fbf9b582f
jwmcmillen <jwmcmillen@gmail.com>
2014-09-25 12:00:40 -0400

    Initial Commit - Purpose :: Test Pull / Push Configuration - FollowUp :: Pushing More Complete Classes Sunday 28 Sep 2014

---
+++ SWE 400 JJJP/src/domain_model/Goal.java
@@ -0,0 +1,15 @@
+package domain_model;
+/* @author Josh McMillen */
+public class Goal
+{
+	int goalID;
+	long dateStart;
+	long dateEnd;
+	int goal;
+	boolean completed;
+	
+	public Goal()
+	{
+		
+	}
+}
---
+++ SWE 400 JJJP/src/domain_model/GoalList.java
@@ -0,0 +1,29 @@
+package domain_model;
+/* @author Josh McMillen */
+public class GoalList
+{
+	public GoalList()
+	{
+		
+	}
+	
+	public void addGoal()
+	{
+		
+	}
+	
+	public void removeGoal()
+	{
+		
+	}
+	
+	public void getGoal()
+	{
+		
+	}
+	
+	public void updateGoal()
+	{
+		
+	}
+}
