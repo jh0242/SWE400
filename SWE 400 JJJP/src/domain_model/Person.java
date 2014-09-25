9f4f0a920ba3049002e47b393d9d034650cf909d
Patrick Joseph Flanagan <pf5776@cs.ship.edu>
2014-09-25 12:37:49 -0400

    Re-added and updated Person class, adding test class when tests return.

--- SWE 400 JJJP/src/domain_model/Person.java
+++ SWE 400 JJJP/src/domain_model/Person.java
@@ -1,11 +1,65 @@
 package domain_model;
 
+import java.util.Vector;
+
 /**
- * 
  * @author Patrick Joseph Flanagan
  *
  */
 public class Person
 {
-	String name;
+	String name;	 // Non-unique full user name.
+	String password; // Passwords aren't protected. We're Sony now.
+	// This field is lazy-loaded and a null value indicates that it hasn't been loaded.
+	private Vector<Person> friends;
+
+	/**
+	 * Getter / name field.
+	 * @return String name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Setter / name field.
+	 * @param name This user's name. Not assumed to be unique.
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * Getter / password field.
+	 * @return String password, unprotected.
+	 */
+	public String getPassword()
+	{
+		return password;
+	}
+
+	/**
+	 * Setter / password field.
+	 * @param password The user's password, unprotected.
+	 */
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	/**
+	 * Get the friends vector.
+	 * Never returns null. Should create it if it's null.
+	 * TODO Should load from the database!
+	 * @return Vector of Persons, never null, but possibly empty ( :( )
+	 */
+	public Vector<Person> getFriends()
+	{
+		if (friends == null) {
+			friends = new Vector<>();
+		}
+		return friends;
+	}
 }
