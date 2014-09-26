51f89ecef31d14ce70adbee27d58880626e7ae7d
jh0242 <jh0242@mct165s12.cs.ship.edu>
2014-09-26 15:26:19 -0400

    Added ActivityTest to tests and added functionality to Activity.

--- SWE 400 JJJP/src/domain_model/Activity.java
+++ SWE 400 JJJP/src/domain_model/Activity.java
@@ -1,12 +1,96 @@
 package domain_model;
 
-/*
- * Joshua Helman
+/**
+ * 
+ * @author Joshua Helman
+ *
  */
 public class Activity
 {
-	public void create(String name, long date, int time, int caloriesBurned)
+	private String name, date;
+	private int time, caloriesBurned;
+	
+	public Activity(String name, String date, int time, int caloriesBurned)
 	{
-		
+		this.name = name;
+		this.date = date;
+		this.time = time;
+		this.caloriesBurned = caloriesBurned;
+	}
+	
+	/**
+	 * Gets the name of the Activity.
+	 * @return String name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Sets this Activity's name to name.
+	 * @param String name
+	 */
+	public void setName(String newName)
+	{
+		this.name = newName;
+	}
+
+	/**
+	 * Gets the date the Activity was created (MM/DD/YY).
+	 * @return String date
+	 */
+	public String getDate() 
+	{
+		return date;
+	}
+	
+	/**
+	 * Sets this Activity's date to newDate (MM/DD/YY).
+	 * @param String date
+	 */
+	public void setDate(String newDate)
+	{
+		// Checks that the value entered for month is valid ie. 1-12.  May not be needed.
+//		if (newDate.charAt(0) != '0' && newDate.charAt(0) != '1'
+//				&& (newDate.charAt(1) == '0' || newDate.charAt(1) == '1' || newDate.charAt(1) == '2'))
+//			return;
+		this.date = newDate;
+	}
+	
+	/**
+	 * Gets the time that the Activity took to complete.
+	 * @return int time
+	 */
+	public int getTime()
+	{
+		return time;
+	}
+	
+	/**
+	 * Sets this Activity's time to newTime
+	 * @param int time
+	 */
+	public void setTime(int newTime)
+	{
+		this.time = newTime;
+	}
+
+	/**
+	 * Gets the amount of calories burned during the Activity.
+	 * @return int caloriesBurned
+	 */
+	public int getCaloriesBurned()
+	{
+		return caloriesBurned;
+	}
+	
+	/**
+	 * Sets this Activity's caloriesBurned to newCaloriesBurned.
+	 * @param int caloriesBurned
+	 */
+	public void setCaloriesBurned(int newCaloriesBurned)
+	{
+		this.caloriesBurned = newCaloriesBurned;
 	}
 }
