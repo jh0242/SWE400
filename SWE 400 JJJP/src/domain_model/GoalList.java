bf2e14d768d823ba930fdf8cad8a0b64b3f2a6df
jwmcmillen <jwmcmillen@gmail.com>
2014-09-30 12:24:05 -0400

    Updated Goals and GoalsList a bit more Tested Initialization of both objects

--- SWE 400 JJJP/src/domain_model/GoalList.java
+++ SWE 400 JJJP/src/domain_model/GoalList.java
@@ -1,29 +1,35 @@
 package domain_model;
+
+import java.util.Hashtable;
+import java.util.Map;
+
 /* @author Josh McMillen */
 public class GoalList
 {
+	Map<Integer,Goal> goalList;
+	
 	public GoalList()
 	{
-		
+		goalList = new Hashtable<Integer,Goal>();
 	}
 	
-	public void addGoal()
+	public void addGoal(Goal goal)
 	{
-		
+		goalList.put(goal.getGoalID(), goal);
 	}
 	
-	public void removeGoal()
+	public void removeGoal(int goalID)
 	{
-		
+		goalList.remove(goalID);
 	}
 	
-	public void getGoal()
+	public Goal getGoal(int goalID)
 	{
-		
+		return goalList.get(goalID);
 	}
 	
-	public void updateGoal()
+	public void updateGoal(Goal goal)
 	{
-		
+		goalList.put(goal.getGoalID(), goal);
 	}
 }
