029928215458331dc70ecc510bbb8b87039398e1
jh0242 <jh0242@mct165s12.cs.ship.edu>
2014-10-03 16:16:10 -0400

    Just added a comment to the insert method in PersonGateway.

--- SWE 400 JJJP/data_source/data_gateway/PersonGateway.java
+++ SWE 400 JJJP/data_source/data_gateway/PersonGateway.java
@@ -18,6 +18,16 @@ public class PersonGateway
 		return 0;
 	}
 	
+	/**
+	 * Checks that the userName isn't already in use in the database, since userName is unique.  If it
+	 * is not in use, we add the new User to the PERSON table in the database and return true.  If it is
+	 * in use, we return false to alert the User that the userName is already in use.
+	 * @param userName
+	 * @param password
+	 * @param displayName
+	 * @return boolean, whether or not the name is valid (true if the name is not in use, else false).
+	 * @throws SQLException
+	 */
 	public boolean insert(String userName, String password, String displayName) throws SQLException
 	{
 		String checkUniqueUserName = new String("SELECT * FROM PERSON where User Name = '" + userName + "';");
