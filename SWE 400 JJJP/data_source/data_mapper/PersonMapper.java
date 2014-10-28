3a846ff8c5f2d36bf3c5423b4ffe38b94e023934
jwmcmillen <jwmcmillen@gmail.com>
2014-10-28 12:48:11 -0400

    Initialized PersonMapper

--- SWE 400 JJJP/data_source/data_mapper/PersonMapper.java
+++ SWE 400 JJJP/data_source/data_mapper/PersonMapper.java
@@ -1,6 +1,41 @@
 package data_mapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import data_gateway.PersonGateway;
+import domain_model.Person;
 
 public class PersonMapper
 {
-
+	HashMap<Integer,Person> users = new HashMap<Integer,Person>();
+	
+	public Person getPerson(int userID) throws SQLException
+	{
+		if(checkUserLoaded(userID)){
+			return users.get(userID);
+		}else if(PersonGateway.userIDisInTable(userID)){
+			return loadUser(userID);
+		}else{
+			return null;
+		}
+	}
+	
+	public Person loadUser(int userID)
+	{
+		ResultSet record = PersonGateway.selectUser(userID);
+		Person loadedUser = new Person(userID);
+		loadedUser.setDisplayName(record.getString("DisplayName"));
+		loadedUser.setPassword(record.getString("Password"));
+		loadedUser.setUsername("UserName");
+		users.put(userID, loadedUser);
+	}
+	
+	public boolean checkUserLoaded(int userid){
+		if(users.containsKey(userid))
+		{
+			return true;
+		}
+		return false;
+	}
 }
