99508cf8365db0d01750854e65fbdcf4808b43df
Patrick Joseph Flanagan <pf5776@cs.ship.edu>
2014-10-28 12:46:06 -0400

    Created a Session object that uses threadlocal. Also tested it.

---
+++ SWE 400 JJJP/tests/SessionTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import domain_model.Person;
+import domain_model.Session;
+
+/**
+ * @author Patrick Joseph Flanagan
+ *
+ */
+public class SessionTest
+{
+	/**
+	 * Test creation of session objects.
+	 */
+	@Test
+	public void testSessionCreation() {
+		Session s = Session.getInstance();
+		Person p = s.getPerson();
+		assertNull(p);
+		p = new Person(1337);
+		s.setPerson(p);
+		assertNotNull(s.getPerson());
+	}
+}
