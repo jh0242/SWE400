329d77d91ffeb688b6a8faeee5c1737811f532e9
jh0242 <jh0242@mct165s10.cs.ship.edu>
2014-11-06 12:57:01 -0500

    Added javadocs to DataBaseConnection to remove warnings.

--- SWE 400 JJJP/data_source/data_gateway/DataBaseConnection.java
+++ SWE 400 JJJP/data_source/data_gateway/DataBaseConnection.java
@@ -3,14 +3,26 @@ package data_gateway;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.sql.DataSource;
 
+/**
+ * Creates a connection to the database for the gateways.
+ * @author jh0242
+ */
 public class DataBaseConnection 
 {
 	private static DataBaseConnection db = null;
 
+	/**
+	 * The destination where the database is held.
+	 */
 	public static final String LOCATION = "jdbc:mysql://lsagroup3.cbzhjl6tpflt.us-east-1.rds.amazonaws.com/fitness3";
+	/**
+	 * The login name to get access to the database.
+	 */
 	public static final String LOGIN_NAME = "lsagroup3";
+	/**
+	 * The password associated with the login name.
+	 */
 	public static final String PASSWORD = "lsagroup3";
 
 	private Connection conn = null;
@@ -34,6 +46,10 @@ public class DataBaseConnection
 		}
 	}
 	
+	/**
+	 * Gets the instance of the DataBaseConnection.
+	 * @return DataBaseConnection
+	 */
 	public static DataBaseConnection getInstance()
 	{
 		if (db == null)
@@ -45,6 +61,7 @@ public class DataBaseConnection
 	/**
 	 * If there is no connection to the database it will create one. If there is
 	 * a connection already established it will return that connection.
+	 * @return Connection
 	 * @throws SQLException 
 	 */
 	public Connection getConnection() throws SQLException
