de8084d24de6b91b467ebe50e8a358ba1b24a544
John <John@Johns-Toshiba>
2014-11-10 10:16:12 -0500

    Added test CommandToPersistChanges

---
+++ SWE 400 JJJP/tests/CommandToPersistChangesTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import domainLogic.CommandToModifyUser;
+import domainLogic.CommandToPersistChanges;
+import domain_model.Person;
+import domain_model.Session;
+
+
+public class CommandToPersistChangesTest {
+
+
+	/**
+	 * Test the functionality CommandToPersistChanges
+	 */
+	@Test
+	public void test()
+	{
+		Person p = new Person(0, "test", "testpwd", "testdisplay");
+		CommandToModifyUser c = new CommandToModifyUser(p.getID(), "nottest"); // It's still test
+		Session.getInstance().setPerson(p);
+		assertEquals(Session.getInstance().getPerson().getFullname(), "testdisplay");
+		
+		CommandToPersistChanges persist = new CommandToPersistChanges();
+		c.execute();
+		persist.execute();
+		assertEquals(Session.getInstance().getPerson().getFullname(), "nottest");
+	}
+
+}
